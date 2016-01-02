49e02e43889fdb8b7dea8d1128cf2e75d08875fd
VegK <eal.prog@gmail.com>
2016-01-02 21:20:49 +1100

    Field control borders and background :black_large_square:

---
+++ Assets/Prefabs/Field/Editor/FieldControllerEditor.cs
@@ -0,0 +1,96 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FieldController))]
+public class FieldControllerEditor : Editor
+{
+	private FieldController _field;
+	private int _lastWidth = -1;
+	private int _lastHeight = -1;
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		_field = target as FieldController;
+
+		if (_field.Width != _lastWidth || _field.Height != _lastHeight)
+		{
+			ResizeBorders();
+			ResizeBackground();
+		}
+		_lastWidth = _field.Width;
+		_lastHeight = _field.Height;
+	}
+
+	private void ResizeBorders()
+	{
+		Vector2 size, pos;
+
+		if (_field.BorderTop != null)
+		{
+			size = _field.BorderTop.size;
+			size.x = _field.Width;
+			_field.BorderTop.size = size;
+
+			pos = _field.BorderTop.transform.position;
+			pos.x = _field.Width / 2f - 0.5f;
+			pos.y = _field.Height;
+			_field.BorderTop.transform.position = pos;
+		}
+
+		if (_field.BorderRight != null)
+		{
+			size = _field.BorderRight.size;
+			size.y = _field.Height;
+			_field.BorderRight.size = size;
+
+			pos = _field.BorderRight.transform.position;
+			pos.x = _field.Width;
+			pos.y = _field.Height / 2f - 0.5f;
+			_field.BorderRight.transform.position = pos;
+		}
+
+		if (_field.BorderBottom != null)
+		{
+			size = _field.BorderBottom.size;
+			size.x = _field.Width;
+			_field.BorderBottom.size = size;
+
+			pos = _field.BorderBottom.transform.position;
+			pos.x = _field.Width / 2f - 0.5f;
+			pos.y = -1;
+			_field.BorderBottom.transform.position = pos;
+		}
+
+		if (_field.BorderLeft != null)
+		{
+			size = _field.BorderLeft.size;
+			size.y = _field.Height;
+			_field.BorderLeft.size = size;
+
+			pos = _field.BorderLeft.transform.position;
+			pos.x = -1;
+			pos.y = _field.Height / 2f - 0.5f;
+			_field.BorderLeft.transform.position = pos;
+		}
+	}
+
+	private void ResizeBackground()
+	{
+		if (_field.Background == null)
+			return;
+
+		Vector3 vec;
+
+		vec = _field.Background.transform.localScale;
+		vec.x = _field.Width;
+		vec.y = _field.Height;
+		_field.Background.transform.localScale = vec;
+
+		vec = _field.Background.transform.position;
+		vec.x = _field.Width / 2f - 0.5f;
+		vec.y = _field.Height / 2f - 0.5f;
+		_field.Background.transform.position = vec;
+	}
+}
---
+++ Assets/Prefabs/Field/FieldController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldController : MonoBehaviour
+{
+	[Header("Game objects")]
+	public BoxCollider2D BorderTop;
+	public BoxCollider2D BorderRight;
+	public BoxCollider2D BorderBottom;
+	public BoxCollider2D BorderLeft;
+	[Space(5)]
+	public GameObject Background;
+	[Header("Settings")]
+	[Range(3, 50)]
+	public int Width = 13;
+	[Range(3, 50)]
+	public int Height = 13;
+
+	private void OnDrawGizmos()
+	{
+		// Draw grid
+		Gizmos.color = Color.white;
+		for (int x = 0; x < Width; x++)
+			for (int y = 0; y < Height; y++)
+			{
+				var pos = transform.position;
+				pos.x += x;
+				pos.y += y;
+				Gizmos.DrawWireCube(pos, Vector2.one);
+			}
+	}
+}
