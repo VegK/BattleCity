8d50bd553b86df3b75ba20dd1fa408e337ef8e2c
VegK <eal.prog@gmail.com>
2016-01-06 15:19:43 +1100

    :zap: shield player

--- Assets/Prefabs/Player/Scripts/PlayerController.cs
+++ Assets/Prefabs/Player/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 public class PlayerController : BlockController
 {
@@ -7,6 +8,32 @@ public class PlayerController : BlockController
 	public bool Lock = false;
 
 	public SpawnController PrefabSpawn;
+	public ShieldController PrefabShield;
+
+	private ShieldController _shield;
+
+	public void ActiveShield(float time)
+	{
+		tag = "Shield";
+
+		_shield = Instantiate(PrefabShield);
+		_shield.transform.position = transform.position;
+		_shield.transform.SetParent(transform, false);
+
+		_shield.TimeLife = time;
+		_shield.DestroyEvent += DestroyShield;
+	}
+
+	public void DeactiveShield()
+	{
+		tag = "Player";
+
+		if (_shield != null)
+		{
+			Destroy(_shield.gameObject);
+			_shield = null;
+		}
+	}
 
 	private void Start()
 	{
@@ -20,5 +47,12 @@ public class PlayerController : BlockController
 	private void DestroySpawn(object sender, EventArgs e)
 	{
 		gameObject.SetActive(true);
+		ActiveShield(1.5f);
+	}
+
+	private void DestroyShield(object sender, EventArgs e)
+	{
+		_shield = null;
+		DeactiveShield();
 	}
 }
---
+++ Assets/Prefabs/Player/Scripts/ShieldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour
+{
+	public float TimeLife = 3.5f;
+
+	public event EventHandler DestroyEvent;
+
+	private void Start()
+	{
+		Destroy(gameObject, TimeLife);
+	}
+
+	private void OnDestroy()
+	{
+		if (DestroyEvent != null)
+			DestroyEvent(this, EventArgs.Empty);
+	}
+}
