2717b86e730af7179104a2560e538f0761134cae
VegK <eal.prog@gmail.com>
2016-01-09 20:43:22 +1100

    Main screen :city_sunrise:

---
+++ Assets/Scripts/GUI/MainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+	public GameObject Cursor;
+
+	public void OnClickPlayer1()
+	{
+		SceneManager.LoadScene("Standard");
+	}
+
+	public void OnClickPlayer2()
+	{
+
+	}
+
+	public void OnClickConstruction()
+	{
+		SceneManager.LoadScene("Editor");
+	}
+
+	public void OnPointerEnter(BaseEventData eventData)
+	{
+		var data = eventData as PointerEventData;
+		if (data == null)
+			return;
+
+		var pos = Cursor.transform.localPosition;
+		pos.y = data.pointerEnter.transform.localPosition.y - 0.6f;
+		Cursor.transform.localPosition = pos;
+	}
+}
