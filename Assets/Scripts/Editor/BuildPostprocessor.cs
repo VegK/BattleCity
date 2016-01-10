4e88a9a1b266d4ab1ccefe8b79c451b71235ae0d
VegK <eal.prog@gmail.com>
2016-01-10 22:57:24 +1100

    Copy files levels postprocess build

---
+++ Assets/Scripts/Editor/BuildPostprocessor.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class BuildPostprocessor
+{
+	[PostProcessBuild(1)]
+	public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+	{
+		var path = Path.GetDirectoryName(pathToBuiltProject);
+		var fileName = Path.GetFileNameWithoutExtension(pathToBuiltProject);
+		var to = path + "/" + fileName + "_Data/Levels/";
+		Directory.CreateDirectory(to);
+
+		var files = new DirectoryInfo(Consts.PATH).GetFiles("*." + Consts.EXTENSION);
+		foreach (FileInfo file in files)
+			FileUtil.CopyFileOrDirectory(Consts.PATH + file.Name, to + file.Name);
+	}
+}
