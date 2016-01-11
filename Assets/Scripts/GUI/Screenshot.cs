33b45b378c75d5e43672c3722f2e72251f14e460
VegK <eal.prog@gmail.com>
2016-01-11 22:24:59 +1100

    Class for create screenshots

---
+++ Assets/Scripts/GUI/Screenshot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace GUI
+{
+	public static class Screenshot
+	{
+		private static Vector2 _position;
+		private static Vector2 _size;
+
+		public static byte[] GetScreenshot(out int height, out int width)
+		{
+			_position = FieldController.Instance.transform.position;
+			_position.x -= 0.5f;
+			_position.y -= 0.5f;
+			_position = Camera.main.WorldToScreenPoint(_position);
+
+			_size = new Vector2();
+			_size.x = FieldController.Instance.Width - 0.5f;
+			_size.y = FieldController.Instance.Height - 0.5f;
+			_size = Camera.main.WorldToScreenPoint(_size);
+			_size -= _position;
+
+			var renderTexture = RenderTexture.GetTemporary(Screen.width, Screen.height);
+			RenderTexture.active = renderTexture;
+			Camera.main.targetTexture = renderTexture;
+			Camera.main.Render();
+
+			width = (int)_size.x;
+			height = (int)_size.y;
+
+			var screenshot = new Texture2D(width, height);
+			screenshot.ReadPixels(new Rect(_position, _size), 0, 0);
+			screenshot.Apply();
+
+			Camera.main.targetTexture = null;
+			Camera.main.Render();
+
+			return screenshot.EncodeToPNG();
+		}
+	}
+}
