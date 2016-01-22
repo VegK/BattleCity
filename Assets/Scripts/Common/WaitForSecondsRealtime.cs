1a6d188b6ddf766c73abf550cc5981ab55d4b3fa
VegK <eal.prog@gmail.com>
2016-01-22 16:09:21 +1100

    WaitForSecondsRealtime

---
+++ Assets/Scripts/Common/WaitForSecondsRealtime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WaitForSecondsRealtime : CustomYieldInstruction
+{
+	private float _waitTime;
+
+	public override bool keepWaiting
+	{
+		get
+		{
+			return (Time.realtimeSinceStartup < _waitTime);
+		}
+	}
+
+	public WaitForSecondsRealtime(float time)
+	{
+		_waitTime = Time.realtimeSinceStartup + time;
+	}
+}
