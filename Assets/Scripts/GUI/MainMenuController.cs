95a16c2950d365d660da705ae0d10feb599d9034
VegK <vegkgh@gmail.com>
2016-04-15 17:12:04 +0300

    Menu item Game Network

--- Assets/Scripts/GUI/MainMenuController.cs
+++ Assets/Scripts/GUI/MainMenuController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -17,13 +18,27 @@ namespace BattleCity.GUI.Main
 		[SerializeField]
 		private Text ScorePlayer2;
 
+		[Header("Menu")]
 		[SerializeField]
 		private GameObject Cursor;
+		[SerializeField]
+		private GameObject MainMenu;
+		[SerializeField]
+		private GameObject NetworkMenu;
+
+		[Header("Input Name Room")]
+		[SerializeField]
+		private GameObject PanelInputNameRoom;
+		[SerializeField]
+		private Text NameRoom;
+		[SerializeField]
+		private Text CaptionButtonOk;
 
 		public static bool Lock { get; set; }
 
 		private static MainMenuController _instance;
 		private static bool _emerge;
+		private InputNameRoomMode _modeInput;
 
 		public static void Show(int scorePlayer1, int scorePlayer2)
 		{
@@ -48,6 +63,7 @@ namespace BattleCity.GUI.Main
 			_instance.gameObject.SetActive(false);
 		}
 
+		#region Main menu methods
 		public void OnClickPlayer1()
 		{
 			if (_emerge)
@@ -64,6 +80,19 @@ namespace BattleCity.GUI.Main
 			GameManager.StartGame(false, (s, e) => { Hide(); });
 		}
 
+		public void OnClickGameNetwork()
+		{
+			if (_emerge)
+				return;
+
+			MainMenu.SetActive(false);
+			NetworkMenu.SetActive(true);
+
+			var select = NetworkMenu.transform.GetChild(0);
+			if (select != null)
+				EventSystem.current.SetSelectedGameObject(select.gameObject);
+		}
+
 		public void OnClickConstruction()
 		{
 			if (_emerge)
@@ -71,6 +100,67 @@ namespace BattleCity.GUI.Main
 			Lock = true;
 			SceneManager.LoadScene("Editor");
 		}
+		#endregion
+		#region Network menu methods
+		public void OnClickCreateGameNetwork()
+		{
+			Lock = true;
+			_modeInput = InputNameRoomMode.Create;
+
+			NameRoom.text = string.Empty;
+			CaptionButtonOk.text = "Create";
+			PanelInputNameRoom.SetActive(true);
+		}
+
+		public void OnClickJoinGameNetwork()
+		{
+			Lock = true;
+			_modeInput = InputNameRoomMode.Join;
+
+			NameRoom.text = string.Empty;
+			CaptionButtonOk.text = "Join";
+			PanelInputNameRoom.SetActive(true);
+		}
+
+		public void OnClickBackMainMenu()
+		{
+			MainMenu.SetActive(true);
+			NetworkMenu.SetActive(false);
+
+			var select = MainMenu.transform.GetChild(0);
+			if (select != null)
+				EventSystem.current.SetSelectedGameObject(select.gameObject);
+		}
+
+		public void OnClickInputNameRoomOk()
+		{
+			switch (_modeInput)
+			{
+				case InputNameRoomMode.Create:
+					Debug.Log("create");
+					break;
+				case InputNameRoomMode.Join:
+					Debug.Log("join");
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+
+			Lock = true;
+			PanelInputNameRoom.SetActive(false);
+		}
+
+		public void OnClickInputNameRoomCancel()
+		{
+			Lock = false;
+			PanelInputNameRoom.SetActive(false);
+
+			var select = NetworkMenu.transform.GetChild(0);
+			if (select != null)
+				EventSystem.current.SetSelectedGameObject(select.gameObject);
+		}
+
+		#endregion
 
 		public void OnPointerEnter(BaseEventData eventData)
 		{
@@ -107,6 +197,13 @@ namespace BattleCity.GUI.Main
 			_instance = this;
 		}
 
+		private void Start()
+		{
+			MainMenu.SetActive(true);
+			NetworkMenu.SetActive(false);
+			PanelInputNameRoom.SetActive(false);
+		}
+
 		private void OnEnable()
 		{
 			Lock = false;
@@ -117,6 +214,12 @@ namespace BattleCity.GUI.Main
 		{
 			if (_emerge && Input.anyKeyDown)
 				ControlEmerge.SetFinishPosition();
+		}
+
+		public enum InputNameRoomMode
+		{
+			Create,
+			Join
 		}
 	}
 }
